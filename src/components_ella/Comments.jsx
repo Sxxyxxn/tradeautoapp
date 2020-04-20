405750e0a4e3f0565aa16c86b99345a1c5da70c3
leejisun <leejisun@leejisunui-MacBook-Pro.local>
2020-04-20 16:53:53 +1200

    delete comment

--- src/components_ella/Comments.jsx
+++ src/components_ella/Comments.jsx
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
+import Axios from "axios";
+import * as UTILS from "../utils";
 
 export default class Comments extends Component {
   constructor(props) {
     super(props);
+    this.state = { comments: [] };
     // comments arrive as props, from parent component
     console.log(this.props);
   }
+
+  removeComment = (e) => {
+    var commentid = e.target.getAttribute("data-id");
+    Axios.delete(`${UTILS.comments_url}/${commentid}`).then((res) => {
+      console.log(res.data);
+      console.log("Deleted");
+      this.refreshData();
+    });
+  };
+
+  componentDidMount() {
+    this.refreshData();
+  }
+
+  refreshData = () => {
+    Axios.get(`${UTILS.comments_url}`).then(
+      (res) => {
+        this.setState({ comments: res.data });
+      },
+      (error) => {
+        console.log("error = ", error);
+      }
+    );
+  };
+
   render() {
     return (
       <div className="showing-comment-container-e">
@@ -16,8 +44,17 @@ export default class Comments extends Component {
           {this.props.comments.map((item, i) => {
             return (
               <div className="showing-comment-e" key={i}>
-                <p>{item.comment}</p>
-                <p>model: {item.car_name}</p>
+                <div>
+                  <p>{item.comment}</p>
+                  {/* <p>model: {item.car_name}</p> */}
+                </div>
+                <button
+                  onClick={this.removeComment}
+                  data-id={item._id}
+                  className="delete-btn-small-e"
+                >
+                  Delete
+                </button>
               </div>
             );
           })}
